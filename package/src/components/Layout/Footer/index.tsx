98e7fdbef73f880d768882c09a669c2dc009d01f
ingsunny <sunnypatel.koder@gmail.com>
2025-10-06 17:35:02 +0530

    changes

--- package/src/components/Layout/Footer/index.tsx
+++ package/src/components/Layout/Footer/index.tsx
@@ -71,7 +71,7 @@ const Footer = () => {
         </div>
         <div className="flex justify-between md:flex-nowrap flex-wrap items-center py-6 gap-6">
           <p className="text-white/40 text-sm ">
-            ©2025 Homely - Design & Developed by <Link href="https://getnextjstemplates.com/" className="hover:text-primary" target="_blanck">GetNextJs Templates</Link>
+            ©2025 Jupiterrealty - Design & Developed by <Link href="https://appmontize.co.in/" className="hover:text-primary" target="_blanck">Appmontize</Link>
           </p>
           <div className="flex gap-8 items-center">
             <Link href="#" className="text-white/40 hover:text-primary text-sm">
